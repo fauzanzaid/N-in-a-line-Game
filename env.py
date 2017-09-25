3159b9e41a3bf783525d2f861540c54146851ff0
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-25 22:27:30 +0530

    Create classes Env and State

---
+++ env.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python2
+
+
+
+class State(object):
+	"""docstring for State"""
+	def __init__(self, arg):
+		self.arg = arg
+		
+
+
+class Env(object):
+	"""docstring for Env"""
+	def __init__(self, arg):
+		self.arg = arg
