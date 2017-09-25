86752783c50d438efc602a3d0a18e5d30216574a
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-25 22:40:14 +0530

    Implement create_grid() in State, use it in Env

--- env.py
+++ env.py
@@ -4,12 +4,26 @@
 
 class State(object):
 	"""docstring for State"""
-	def __init__(self, arg):
-		self.arg = arg
+
+	TILE_EMPTY = 0
+	TILE_A = 1
+	TILE_B = 2
+
+
+	def __init__(self, dim):
+		self.dim = dim
+		self.grid = None
+
+		self.create_grid(self.dim)
+
+
+	def create_grid(self, dim):
+		self.grid = [ [self.TILE_EMPTY]*dim[1] for i in dim[0] ]
 		
 
 
 class Env(object):
 	"""docstring for Env"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self, dim):
+		self.dim = dim
+		self.state = State(self.dim)
