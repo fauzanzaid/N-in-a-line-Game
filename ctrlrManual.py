674e79db8628a764d942d398d50ef58ee17e094f
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-26 18:37:13 +0530

    Implement output(), which uses get_pos() to get desired position

--- ctrlrManual.py
+++ ctrlrManual.py
@@ -4,7 +4,11 @@ from ctrlr import Controller
 
 class ControllerManual(Controller):
 	"""docstring for ControllerManual"""
-	def __init__(self, arg):
-		super(ControllerManual, self).__init__()
-		self.arg = arg
-		
+	def __init__(self, name, get_pos):
+		super(ControllerManual, self).__init__(name)
+		self.get_pos = get_pos
+
+
+	def output(self, state):
+		pos = self.get_pos(state)
+		return (self.player_ordinality, pos)
