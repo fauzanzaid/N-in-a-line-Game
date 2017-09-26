2320d3b44f8ebbd23244f65a3d0bd759bc7e411c
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-26 18:32:55 +0530

    Add variable player_ordinality to contain information about player being A or B

--- ctrlr.py
+++ ctrlr.py
@@ -2,9 +2,9 @@
 
 class Controller(object):
 	"""docstring for Controller"""
-	def __init__(self, name):
+	def __init__(self, name, player_ordinality):
 		self.name = name
-
+		self.player_ordinality = player_ordinality
 
 	def output(self, state):
 		raise NotImplementedError()
