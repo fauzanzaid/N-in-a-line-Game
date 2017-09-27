d71f3dcb2e0c91dbc3a40c2b1e4b3a90b5736eeb
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-27 23:14:22 +0530

    Implement output()

--- ctrlrMinMax.py
+++ ctrlrMinMax.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python2
 
 from ctrlr import Controller
+from state import State
 
 class ControllerMinMax(Controller):
 	"""docstring for ControllerMinMax"""
@@ -14,7 +15,21 @@ class ControllerMinMax(Controller):
 
 
 	def output(self, state):
-		pass
+		move_positions = State.get_move_postions(state)
+		utivals = []
+
+		for pos in move_positions:
+			state_new = get_state_on_move(state, (self.player_ordinality, pos))
+			utival = minmax()
+			utivals.append(utival)
+
+		pos = None
+		if player_ordinality == State.PLAYER_A:
+			pos = move_positions[ utivals.index(max(utivals)) ]
+		else:
+			pos = move_positions[ utivals.index(max(utivals)) ]
+
+		return (player_ordinality, pos)
 
 
 	def minmax(self, state):
