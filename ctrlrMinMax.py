247e96b9b2722fb06a3f2caace4efb30cccb3780
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-27 22:58:19 +0530

    Create function prototypes

--- ctrlrMinMax.py
+++ ctrlrMinMax.py
@@ -4,6 +4,18 @@ from ctrlr import Controller
 
 class ControllerMinMax(Controller):
 	"""docstring for ControllerMinMax"""
-	def __init__(self, arg):
-		super(ControllerMinMax, self).__init__()
-		self.arg = arg
+
+	UTIVAL_DRAW = 0
+	UTIVAL_WIN_A = 1
+	UTIVAL_WIN_B = -1
+
+	def __init__(self, , name, player_ordinality):
+		super(ControllerMinMax, self).__init__(name, player_ordinality)
+
+
+	def output(self, state):
+		pass
+
+
+	def minmax(self, state):
+		pass
