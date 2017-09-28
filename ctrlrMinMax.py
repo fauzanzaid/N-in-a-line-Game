17bde82010d6b6fa8cf6d0ecbf1e7886f8a5b284
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-28 07:19:29 +0530

    Implement minmax()

--- ctrlrMinMax.py
+++ ctrlrMinMax.py
@@ -10,27 +10,59 @@ class ControllerMinMax(Controller):
 	UTIVAL_WIN_A = 1
 	UTIVAL_WIN_B = -1
 
-	def __init__(self, , name, player_ordinality):
+	def __init__(self, name, player_ordinality):
 		super(ControllerMinMax, self).__init__(name, player_ordinality)
 
 
 	def output(self, state):
-		move_positions = State.get_move_postions(state)
+		move_positions = State.get_move_positions(state)
 		utivals = []
 
 		for pos in move_positions:
-			state_new = get_state_on_move(state, (self.player_ordinality, pos))
-			utival = minmax()
+			state_new = State.get_state_on_move(state, (self.player_ordinality, pos))
+			utival = self.minmax(state_new)
 			utivals.append(utival)
 
 		pos = None
-		if player_ordinality == State.PLAYER_A:
+		if self.player_ordinality == State.PLAYER_A:
 			pos = move_positions[ utivals.index(max(utivals)) ]
 		else:
 			pos = move_positions[ utivals.index(max(utivals)) ]
 
-		return (player_ordinality, pos)
+		return (self.player_ordinality, pos)
 
 
 	def minmax(self, state):
-		pass
+		if State.is_state_aligned(state):
+			if state.player_last == State.PLAYER_A:
+				return self.UTIVAL_WIN_A
+			else:
+				return self.UTIVAL_WIN_B
+
+		elif State.is_state_full(state):
+			return self.UTIVAL_DRAW
+
+		else:
+			utival_best = None
+			player_cur = None
+
+			if state.player_last == State.PLAYER_A:
+				utival_best = float("inf")
+				player_cur = State.PLAYER_B
+			else:
+				utival_best = float("-inf")
+				player_cur = State.PLAYER_A
+
+			move_positions = State.get_move_positions(state)
+			for pos in move_positions:
+				state_new = State.get_state_on_move(state, (player_cur,pos))
+				utival = self.minmax(state_new)
+
+				if player_cur == State.PLAYER_A:
+					if utival > utival_best:
+						utival_best = utival
+				else:
+					if utival < utival_best:
+						utival_best = utival
+
+			return utival_best
