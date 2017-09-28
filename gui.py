d0e24522e4a03362d462f02305e58df0a8076527
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-28 09:47:34 +0530

    Calculate partition coods and dimensions

--- gui.py
+++ gui.py
@@ -4,5 +4,32 @@ import turtle
 
 class GUI(object):
 	"""docstring for GUI"""
-	def __init__(self):
-		pass
+	def __init__(self, dim):
+
+		self.game_dim = dim
+
+		self.P1_PAD = 20
+		self.P1_HT = self.P1_PAD*2 + 20*12
+		self.P1_WD = self.P1_PAD*2 + 200
+
+		self.P2_PAD = 20
+		self.P2_HT = self.P2_PAD*2 + 40*dim[0] - 40
+		self.P2_WD = self.P2_PAD*2 + 40*dim[1] - 40
+
+		self.P3_PAD = 20
+		self.P3_HT = self.P3_PAD*2 + 20
+		self.P3_WD = self.P3_PAD*2 + 600
+
+		self.P1_COOD_0 = self.P1_PAD + max(0, self.P2_HT - self.P1_HT)/2.0
+		self.P1_COOD_1 = self.P1_PAD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+
+		self.P2_COOD_0 = self.P2_PAD + max(0, self.P1_HT - self.P2_HT)/2.0
+		self.P2_COOD_1 = self.P2_PAD + self.P1_WD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+
+		self.P3_COOD_0 = self.P3_PAD + max(self.P1_HT, self.P2_HT)
+		self.P3_COOD_1 = self.P3_PAD + max(0, self.P1_WD + self.P2_WD - self.P3_WD)/2.0
+
+		self.WINDOW_HT = max(self.P1_HT, self.P2_HT) + self.P3_HT
+		self.WINDOW_WD = max(self.P1_WD + self.P2_WD, self.P3_WD)
+
+		turtle.title("Align three")
