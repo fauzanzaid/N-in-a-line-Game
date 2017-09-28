c50ca63ec2ea3b167e8ff31c673db54a5e5db3bb
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-09-28 14:50:04 +0530

    Change cood sys, lower left is 0,0. Implement draw_tile()

--- gui.py
+++ gui.py
@@ -8,35 +8,50 @@ class GUI(object):
 
 		self.game_dim = dim
 
+		self.TILE_SIZE = 40
+		self.COIN_SIZE = 30
+		self.COIN_COL_A = (1,0,0)
+		self.COIN_COL_B = (0,1,0)
+
 		self.P1_PAD = 20
 		self.P1_HT = self.P1_PAD*2 + 20*12
 		self.P1_WD = self.P1_PAD*2 + 200
 
 		self.P2_PAD = 20
-		self.P2_HT = self.P2_PAD*2 + 40*dim[0] - 40
-		self.P2_WD = self.P2_PAD*2 + 40*dim[1] - 40
+		self.P2_HT = self.P2_PAD*2 + self.TILE_SIZE*(dim[0]-1)
+		self.P2_WD = self.P2_PAD*2 + self.TILE_SIZE*(dim[1]-1)
 
 		self.P3_PAD = 20
 		self.P3_HT = self.P3_PAD*2 + 20
 		self.P3_WD = self.P3_PAD*2 + 600
 
-		self.P1_COOD_0 = self.P1_PAD + max(0, self.P2_HT - self.P1_HT)/2.0
-		self.P1_COOD_1 = self.P1_PAD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+		self.P1_COOD_X = + self.P1_PAD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+		self.P1_COOD_Y = - self.P1_PAD + self.P1_HT + self.P3_HT + max(0, self.P2_HT - self.P1_HT)/2.0
 
-		self.P2_COOD_0 = self.P2_PAD + max(0, self.P1_HT - self.P2_HT)/2.0
-		self.P2_COOD_1 = self.P2_PAD + self.P1_WD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+		self.P2_COOD_X = + self.P2_PAD + self.P1_WD + max(0, self.P3_WD - self.P1_WD - self.P2_WD)/2.0
+		self.P2_COOD_Y = - self.P2_PAD + self.P2_HT + self.P3_HT + max(0, self.P1_HT - self.P2_HT)/2.0
 
-		self.P3_COOD_0 = self.P3_PAD + max(self.P1_HT, self.P2_HT)
-		self.P3_COOD_1 = self.P3_PAD + max(0, self.P1_WD + self.P2_WD - self.P3_WD)/2.0
+		self.P3_COOD_X = + self.P3_PAD + max(0, self.P1_WD + self.P2_WD - self.P3_WD)/2.0
+		self.P3_COOD_Y = - self.P3_PAD + self.P3_HT
 
 		self.WINDOW_HT = max(self.P1_HT, self.P2_HT) + self.P3_HT
 		self.WINDOW_WD = max(self.P1_WD + self.P2_WD, self.P3_WD)
 
 		turtle.title("Align three")
 		turtle.setup(width=self.WINDOW_WD, height=self.WINDOW_HT)
-		turtle.setworldcoordinates(0, self.WINDOW_HT, self.WINDOW_WD, 0)
+		turtle.setworldcoordinates(0, 0, self.WINDOW_WD, self.WINDOW_HT)
 
 		turtle.ht()
 		turtle.pu()
 		turtle.speed(0)
 		turtle.delay(0)
+
+
+	def draw_tile(self, cood):
+		turtle.goto(cood[0]-self.TILE_SIZE/2.0, cood[1]-self.TILE_SIZE/2.0)
+		turtle.seth(0)
+		turtle.pd()
+		for i in xrange(4):
+			turtle.fd(self.TILE_SIZE)
+			turtle.lt(90)
+		turtle.pu()
