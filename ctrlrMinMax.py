f478cd455c3c63a9ef229bb9d5cc713f98bb0a11
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-10-01 15:24:17 +0530

    Add support for pre calculated utivals to speed up gameplay

--- ctrlrMinMax.py
+++ ctrlrMinMax.py
@@ -8,22 +8,35 @@ from state import State
 class ControllerMinMax(Controller):
 	"""docstring for ControllerMinMax"""
 
-	def __init__(self, name, player_ordinality):
+	def __init__(self, name, player_ordinality, precalc_utivals_enabled = True):
 		super(ControllerMinMax, self).__init__(name, player_ordinality)
 		self.stats["d"] = 0
 		self.stats["n"] = 0
+
+		self.precalc_utivals_enabled = precalc_utivals_enabled
+
+		if self.precalc_utivals_enabled == True:
+			self.precalc_utivals_dict = {}
+			self.update_precalc_utivals_dict()
 
 
 	def output(self, state):
 		time_init = time.time()
 
 		move_positions = State.get_move_positions(state)
-		utivals = []
 
-		for pos in move_positions:
-			state_new = State.get_state_on_move(state, (self.player_ordinality, pos))
-			utival = self.minmax(state_new, 0)
-			utivals.append(utival)
+		utivals = None
+		if self.precalc_utivals_enabled == True:
+			utivals = self.precalc_utivals_dict.get(state, None)
+
+		if utivals == None:
+			utivals = []
+			for pos in move_positions:
+				state_new = State.get_state_on_move(state, (self.player_ordinality, pos))
+				utival = self.minmax(state_new, 0)
+				utivals.append(utival)
+
+		print tuple(utivals)
 
 		pos = None
 		if self.player_ordinality == State.PLAYER_A:
@@ -69,3 +82,110 @@ class ControllerMinMax(Controller):
 						utival_best = utival
 
 			return utival_best
+
+
+	def update_precalc_utivals_dict(self):
+
+		s = State((4,4),3)
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1,1,1,1)
+
+
+		grid = ((1, 0, 0, 0), (2, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1,1,1,-1)
+
+		grid = ((1, 2, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, -1, -1)
+
+		grid = ((1, 0, 2, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, -1, 1, -1)
+
+		grid = ((1, 0, 0, 2), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, 1, -1)
+
+
+		grid = ((2, 1, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, 1, 1)
+
+		grid = ((0, 1, 0, 0), (0, 2, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, -1)
+
+		grid = ((0, 1, 2, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, -1)
+
+		grid = ((0, 1, 0, 2), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, 1, 1)
+
+
+		grid = ((2, 0, 1, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, 1, 1)
+
+		grid = ((0, 2, 1, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, -1)
+
+		grid = ((0, 0, 1, 0), (0, 0, 2, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, -1)
+
+		grid = ((0, 0, 1, 2), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (1, 1, 1, 1)
+
+
+		grid = ((2, 0, 0, 1), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, 1)
+
+		grid = ((0, 2, 0, 1), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, -1, -1)
+
+		grid = ((0, 0, 2, 1), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, -1, 1, 1)
+
+		grid = ((0, 0, 0, 1), (0, 0, 0, 2), (0, 0, 0, 0), (0, 0, 0, 0))
+		s = State.get_state_from_grid(grid)
+		s.min_length = 3
+		s.player_last = State.PLAYER_B
+		self.precalc_utivals_dict[s] = (-1, 1, 1, 1)
